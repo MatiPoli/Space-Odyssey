906a6de66f1d71c70978a61ef9ae46fdb557b3ba
DonBerge <agustinfb93@gmail.com>
2021-09-01 22:42:23 -0300

    Primera parte del prototipo del juego
    Tiene una sola escena, con un planeta y una capsula que esta afectada por la gravedad del planeta, por ahora no se puede mover.

---
+++ Unity/Assets/Main.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private CharacterController controller;
+    public GameObject planeta; // Planeta actual
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.gravity = planeta.transform.position - transform.position;  // Hace que el vector gravedad siempre apunte al centro del planeta(creo).
+
+        transform.rotation = Quaternion.FromToRotation(transform.up, -Physics.gravity) * transform.rotation; // Tengo que investigar la matematica detras de esto, pero basicamente alinea el eje Y del personaje con el del planeta.
+        
+    }
+}
