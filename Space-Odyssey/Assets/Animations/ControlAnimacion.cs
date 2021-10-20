c4b8338d2a19f40f4145cd27a51a1d3164ed6c86
MatiPoli <matiastmarquez@gmail.com>
2021-10-20 12:23:47 -0300

    Añado a Batracio y sus animaciones

---
+++ Space-Odyssey/Assets/Animations/ControlAnimacion.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlAnimacion : MonoBehaviour
+{
+    Animator animator;
+    public GameObject planeta;
+    private bool enPiso=true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+     void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject == planeta)
+        {
+            enPiso=true;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if(col.gameObject == planeta)
+        {
+            enPiso=false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        bool isJumping = animator.GetBool("isJumping");
+        bool isWalking = animator.GetBool("isWalking");
+        bool forwardPressed = Input.GetKey("w");
+        bool spacePressed = Input.GetKeyDown(KeyCode.Space);
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool("isWalking", true);
+        }
+
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if (isWalking && spacePressed  && !isJumping)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isJumping", true);
+        }
+
+        
+    }
+}
