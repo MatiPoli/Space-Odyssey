803875da3bf4dc265bb497fe24f71db9c48718af
Ramiro089 <ramirogatto089@gmail.com>
2021-10-23 11:01:43 -0300

    Sistema de combustible funcional

--- Space-Odyssey/Assets/Scripts/Combustible_Nave.cs
+++ Space-Odyssey/Assets/Scripts/Combustible_Nave.cs
@@ -1,19 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Combustible_Nave : MonoBehaviour
 {
-    
-
     void Start()
     {
-
+    
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
+    if (Input.GetKey("w")){
+        GetComponent<Image>().fillAmount -= 0.00001f; 
+
+    }
+
+    if( GetComponent<Image>().fillAmount < 0.5f){
+        GetComponent<Image>().color = new Color32(156,95,0,255);
+    }
+
+    if( GetComponent<Image>().fillAmount < 0.25f){
+        GetComponent<Image>().color = new Color32(156,28,0,255);
+    }
+    
+    if( GetComponent<Image>().fillAmount == 0){
+        Debug.Break();
+    }
+
+    
+
     }
 }
