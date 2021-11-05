2d04326c4b5cdabae0c766df5afefe483698558f
DonBerge <agustinfb93@gmail.com>
2021-11-05 01:37:04 -0300

    IA medio funcional

--- Space-Odyssey/Assets/Scripts/Combate/Enemigo.cs
+++ Space-Odyssey/Assets/Scripts/Combate/Enemigo.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 [RequireComponent(typeof(Animator))]
 public class Enemigo : DamageTarget
 {
-    public Animator animator;
+    Animator animator;
     AudioSource death_sound;
+    NavMeshAgent agent;
 
-    new void Start()
+    [Header("Arma")]
+    public Arma arma;
+
+    [Header("Datos del enemigo a vencer")]
+    public LayerMask targetLayer;
+    public Transform target;
+
+    void Awake()
     {
-        base.Start();
         animator = GetComponent<Animator>();
         death_sound = GetComponent<AudioSource>();
+        agent = GetComponent<NavMeshAgent>();
+        //target = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        bool targetEnRango = Physics.CheckSphere(transform.position, arma.attackRange, targetLayer);
+        if (targetEnRango)
+        {
+            agent.SetDestination(transform.position);
+            faceTarget();
+            arma.attack();
+        }
+        else
+            agent.SetDestination(target.position);
+
+    }
+
+    void faceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = lookRotation;
     }
 
     public override void recibirDanio(float danio)
@@ -24,14 +55,19 @@ public class Enemigo : DamageTarget
             animator.SetTrigger("Lastimado");
         }
     }
+    
 
     protected override void die()
     {
+
         // Animacion de Muerte
         animator.SetBool("Lo Nismearon", true);
 
         // Sonido de muerte
         if(death_sound != null)
             death_sound.PlayOneShot(death_sound.clip);
+
+        this.enabled = false;
+
     }
 }
---
+++ Space-Odyssey/Assets/Scripts/Combate/Gun.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : Arma
+{
+    public Transform bulletOrigin;
+    public ParticleSystem muzzleFlash;
+    AudioSource sonidoDisparo;
+
+    void Start()
+    {
+        sonidoDisparo = GetComponent<AudioSource>();
+    }
+
+    void playSonidoDisparo()
+    {
+        sonidoDisparo.PlayOneShot(sonidoDisparo.clip);
+    }
+
+    override public void attack()
+    {
+        if (!attackReady())
+            return;
+
+        // Animacion de disparo
+
+        // Flash
+        muzzleFlash.Play();
+
+        // Sonido de disparo
+        playSonidoDisparo();
+
+        //Deteccion de golpe
+        RaycastHit hit;
+        if (Physics.Raycast(bulletOrigin.transform.position, bulletOrigin.transform.forward, out hit, attackRange))
+        {
+            //Debug.Log("Le pegue un balaso a " + hit.transform.gameObject.name);
+            DamageTarget target = hit.transform.GetComponent<DamageTarget>();
+
+            if (target != null)
+                target.recibirDanio(attackDamage);
+        }
+    }
+}
