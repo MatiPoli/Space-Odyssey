f5a5fbc47ee93bc82ba342fd61466565464cb32d
MatiPoli <matiastmarquez@gmail.com>
2021-11-05 17:05:19 -0300

    markos gordoy

--- Space-Odyssey/Assets/Scripts/Pozo.cs
+++ Space-Odyssey/Assets/Scripts/Pozo.cs
@@ -8,6 +8,7 @@ public class Pozo : MonoBehaviour
 	private float vidaActual;
 	private float tiempo = 0.0f;
 
+	/*
 	private void OnCollisionStay(Collision collision) //la entrada es un collision, objecto que entra en colision
     {
 
@@ -21,14 +22,34 @@ public class Pozo : MonoBehaviour
     		//accedo a la vida que esta en damageTarget
     		vidaActual = jugador.GetComponent<Variables>().getVida(); 
 
-    		if(tiempo <= 2 || tiempo >= 5) {
-    			vidaActual = vidaActual/2;
+    		if(tiempo >= 15.0f) {
+    			vidaActual = vidaActual - 50;
     			jugador.GetComponent<DamageTarget>().actualizarVida(vidaActual);
     		}
 
     	}
 
     }
+	*/
+
+	private void OnCollisionEnter(Collision collision) //la entrada es un collision, objecto que entra en colision
+    {
+
+    	jugador = GameObject.FindWithTag("Player");
+    	GameObject objeto = collision.gameObject;
+
+    	if(jugador == objeto) {
+
+    		//accedo a la vida que esta en damageTarget
+    		vidaActual = jugador.GetComponent<Variables>().getVida(); 
+
+    		vidaActual = vidaActual - 20;
+    		jugador.GetComponent<DamageTarget>().actualizarVida(vidaActual);
+
+    	}
+
+    }
+
 
     // Start is called before the first frame update
     void Start()
