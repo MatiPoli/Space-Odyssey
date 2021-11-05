50cfc1c8f8e7a673fabbff9e465836aca8bf0f58
DonBerge <agustinfb93@gmail.com>
2021-11-04 23:56:06 -0300

    Enemigo con animaciones y sonido

--- Space-Odyssey/Assets/Scripts/Combate/Enemigo.cs
+++ Space-Odyssey/Assets/Scripts/Combate/Enemigo.cs
@@ -2,18 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Enemigo : DamageTarget
 {
+    public Animator animator;
+    AudioSource death_sound;
+
+    new void Start()
+    {
+        base.Start();
+        animator = GetComponent<Animator>();
+        death_sound = GetComponent<AudioSource>();
+    }
+
     public override void recibirDanio(float danio)
     {
-        base.recibirDanio(danio);
-        // Animacion de daño
+        if (vida > 0)
+        {
+            base.recibirDanio(danio);
+            // Animacion de daño
+            animator.SetTrigger("Lastimado");
+        }
     }
 
     protected override void die()
     {
-        Debug.Log(this.name + " ha muerto!");
         // Animacion de Muerte
-        Destroy(this.gameObject);
+        animator.SetBool("Lo Nismearon", true);
+
+        // Sonido de muerte
+        if(death_sound != null)
+            death_sound.PlayOneShot(death_sound.clip);
     }
 }
