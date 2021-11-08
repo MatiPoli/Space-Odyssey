9bc9d947a3ba7a5d3aefd81bb123f46606798ccb
DonBerge <agustinfb93@gmail.com>
2021-11-07 21:02:32 -0300

    Enemigos implementados en la tierra

---
+++ Space-Odyssey/Assets/DebugSphere.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugSphere : MonoBehaviour
+{
+    [Header("Datos")]
+    Vector3 origin;
+    public float range=2f;
+    public Color color=Color.red;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawSphere(this.transform.position, range);
+    }
+}
