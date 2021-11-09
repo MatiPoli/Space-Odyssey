5bcf9e673286ba812987557441a88c922560b038
DonBerge <agustinfb93@gmail.com>
2021-11-08 23:44:43 -0300

    Ahora los enemigos sueltan el arma al morir

--- Space-Odyssey/Assets/Scripts/Gravedad/Gravedad.cs
+++ Space-Odyssey/Assets/Scripts/Gravedad/Gravedad.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Gravedad : MonoBehaviour
 {
     public Atractor atractor;
     Rigidbody rb;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
         rb.constraints = RigidbodyConstraints.FreezeRotation;
