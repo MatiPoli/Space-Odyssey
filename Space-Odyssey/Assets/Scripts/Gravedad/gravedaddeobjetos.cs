c3912e1c105ef6813a69620cf836eda01ff5433e
valentindalmau <valentindalmau1@gmail.com>
2021-11-10 11:05:45 -0300

    gravedad de arboles

---
+++ Space-Odyssey/Assets/Scripts/Gravedad/gravedaddeobjetos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravedaddeobjetos : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject Planeta;
+    void OnCollisionEnter(Collision col)
+    {
+    	if (col.gameObject==Planeta)
+    	{
+    		rb = GetComponent<Rigidbody>();
+    		rb.mass=10000;
+    	}
+    }
+
+	void Start()
+	{
+		
+	}
+}
