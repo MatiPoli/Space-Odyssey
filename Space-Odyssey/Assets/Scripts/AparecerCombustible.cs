e6836d13adbeb3f03cef9947a65b190572c8613c
Marcos-Godoy <mjgodoy2002@gmail.com>
2021-11-11 12:48:01 -0300

    Aparecer el combustible

---
+++ Space-Odyssey/Assets/Scripts/AparecerCombustible.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AparecerCombustible : MonoBehaviour
+{
+
+	public GameObject myPrefab;
+	public Transform respawn;
+	//private Transform pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float distancia;
+
+    	distancia = this.GetComponent<DistEntreObj>().calcularDistancia();
+
+    	if(Input.GetKeyDown("p")){
+	    	if(distancia <= 50.0f){
+	    		GameObject instantiatedObject = Instantiate(myPrefab, respawn.position, respawn.rotation, null);
+	            instantiatedObject.name = "Gasolina";
+	            instantiatedObject.AddComponent<Rigidbody>();
+	            instantiatedObject.GetComponent<Rigidbody>().freezeRotation = true;
+	    	}
+        }
+    }
+}
