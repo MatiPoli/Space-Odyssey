b8e67192410ca4c503ba623bf6f41b203ec4c766
MatiPoli <matiastmarquez@gmail.com>
2021-11-11 23:41:36 -0300

    cambios en el earth

--- Space-Odyssey/Assets/GuardadorDeInventario.cs
+++ Space-Odyssey/Assets/GuardadorDeInventario.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GuardadorDeInventario : MonoBehaviour
 {
     private GameObject slot;
+    public GameObject lista;
+    private GameObject aux;
+    private GameObject aux2;
     private int i;
-
+    private int c;
     void Start()
     {
         if(PlayerPrefs.GetInt("inic3", 0)==1)
@@ -37,6 +42,20 @@ public class GuardadorDeInventario : MonoBehaviour
             slot = transform.GetChild(i).gameObject;
             slot.GetComponent<Slot>().ID = PlayerPrefs.GetInt("sloti"+ i, 0);
             slot.GetComponent<Slot>().cantidad = PlayerPrefs.GetInt("slotc"+ i, 0);
+            for(c=0;c<11;c++)
+            {
+                aux = lista.transform.GetChild(c).gameObject;
+                if(aux.GetComponent<Item>().ID == PlayerPrefs.GetInt("sloti"+ i, 0))
+                {
+                    slot.GetComponent<Slot>().empty = false;
+                    slot.GetComponent<Slot>().item = aux;
+                    slot.GetComponent<Slot>().type = aux.GetComponent<Item>().type;
+                    slot.GetComponent<Slot>().descripcion = aux.GetComponent<Item>().descripcion;
+                    slot.GetComponent<Slot>().icon = aux.GetComponent<Item>().icon;
+                    aux2 = slot.transform.GetChild(0).gameObject;
+                    aux2.GetComponent<Image>().sprite = slot.GetComponent<Slot>().icon;
+                }
+            }
         }
     }
     // Update is called once per frame
